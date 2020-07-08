03c1a56c00c56b2b3c6297693eb1e5f1d0327a03
Nabil Benssadek <benweb31@gmail.com>
2020-07-08 15:26:43 +0200

    ajout d'une fonctionnalité de recherche

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Card from './components/Card';
+import CardSearch from './components/CardSearch';
 import { BounceLoader } from 'react-spinners';
 
 function App() {
   const [cards, setCards] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
 
   useEffect(() => {
     fetch('https://api.magicthegathering.io/v1/cards', {
@@ -12,14 +14,18 @@ function App() {
     })
       .then((res) => res.json())
       .then((data) => {
-        setCards(data.cards);
+        setCards(
+          data.cards.filter((card) => card.name.toLowerCase().includes(term))
+        );
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
+      <CardSearch searchText={(text) => setTerm(text)} />
+
       {isLoading ? (
         <div className="flex justify-center mt-32">
           <BounceLoader loading size={150} color={'#123abc'} />
