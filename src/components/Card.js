fbab42aa7500bdd371b1aade27a0f9bf7a9a1134
Nabil Benssadek <benweb31@gmail.com>
2020-07-09 11:55:21 +0200

    ajustement css pour que le type reste en bas de la carte

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ image, name, rarity, text, types }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col">
       <img src={image} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-pink-500 text-xl mb-2">{name}</div>
@@ -17,7 +17,7 @@ const Card = ({ image, name, rarity, text, types }) => {
           </li>
         </ul>
       </div>
-      <div className="px-6 py-4">
+      <div className="px-6 py-4 mt-auto">
         {types.map((type, index) => (
           <span
             key={index}
